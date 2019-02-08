f2e5ac3676bcbb1a3a52d413395ee4b29a9d55f9
Chris Dzombak <chris@chrisdzombak.net>
2019-02-07 22:48:01 -0500

    Tweak folder structure

--- music/play.sh
+++ music/play.sh
@@ -6,11 +6,11 @@ LAST_FILE=""
 
 while true
 do
-	FILE=$(find playlist/mp3 -type f | shuf -n 1)
+	FILE=$(find ~/music/playlist -type f | shuf -n 1)
 	if [ "$FILE" == "$LAST_FILE" ]; then
 		continue
 	fi
 	LAST_FILE="$FILE"
 
-	ffmpeg -i "$FILE" -filter:a "volume=0.95" -f s16le -ar 22.05k -ac 1 - | sudo ./pifm/pifm - 103.3
+	ffmpeg -i "$FILE" -filter:a "volume=0.95" -f s16le -ar 22.05k -ac 1 - | sudo ~/music/pifm/pifm - 103.3
 done
